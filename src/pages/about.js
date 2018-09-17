07c4bfb38aaf2c2da64f9d38490ebf26ab67c026
Pooja patel <poojavpatel71@gmail.com>
2018-09-18 00:07:47 +0530

    modified about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const AboutPage = () => {
-  return (
+const AboutPage = () => (
     <div>
       <h1>About Page</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores aut optio delectus quas ab? Corrupti reiciendis voluptates optio voluptate alias similique, ab, eaque facere officia adipisci nesciunt vel et excepturi!</p>
     </div>
-  )
-}
+)
 
 export default AboutPage;
