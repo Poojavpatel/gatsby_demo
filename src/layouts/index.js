916307e27c014317acd537709586776a45d7e1a6
Pooja patel <poojavpatel71@gmail.com>
2018-09-17 22:54:49 +0530

    added new file about.js in pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,8 +10,8 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Sample website for gatsby demo' },
+        { name: 'keywords', content: 'gatsby, react' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores aut optio delectus quas ab? Corrupti reiciendis voluptates optio voluptate alias similique, ab, eaque facere officia adipisci nesciunt vel et excepturi!</p>
+    </div>
+  )
+}
+
+export default AboutPage;
